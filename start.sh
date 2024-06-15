746b3f43f9fb9226b37f56164783bd157449830c
EZLJ <chiericzhang@outlook.com>
2024-06-15 05:35:43 +0000

    Backup: 15 Jun 24, 05:35 AM

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 cd /workspaces/Ourcraft/server
 /workspaces/Ourcraft/backup.sh &
 /workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 596222,596223,596224 &
-java -Xmx8G -Xms4G -jar paper-1.20.6-139.jar nogui
+java -Xmx7G -Xms4G -jar /workspaces/Ourcraft/server/paper-1.20.1-196.jar nogui
