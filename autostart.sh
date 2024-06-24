0185cf42a891c1c88f93bd9fa78bd72c09bd653e
PZV5078 <zhangchi2910@163.com>
2024-06-24 07:18:11 +0000

    Backup: 24 Jun 24, 07:18 AM

--- autostart.sh
+++ autostart.sh
@@ -2,5 +2,5 @@ echo Sleeping for 5s...
 sleep 5
 cd /workspaces/Ourcraft/server
 /workspaces/Ourcraft/backup.sh &
-/workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 600739,596223,599718,598383,602133  &
+/workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 598383,603037,603038,603048,603051,603060,603065,603084 &
 java -Xmx7G -Xms4G -jar /workspaces/Ourcraft/server/paper-1.20.1-196.jar nogui
