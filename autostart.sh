a05d3bd28484ddc0bf83ec4e7200b98ab0a4ada0
EZLJ <chiericzhang@outlook.com>
2024-09-09 10:14:12 +0000

    modified:   autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,5 +2,5 @@ echo Sleeping for 5s...
 sleep 5
 cd /workspaces/Ourcraft/server
 /workspaces/Ourcraft/backup.sh &
-/workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 645056,603084,603065,645050   &
-java -Xmx7G -Xms4G -jar /workspaces/Ourcraft/server/paper-1.20.1-196.jar nogui
+/workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 603065, 645050, 645056, 687086 &
+java -Xmx14G -Xms4G -jar /workspaces/Ourcraft/server/paper-1.20.1-196.jar nogui
