2771f1dca39fee81251ad297f10412060233ca0b
ZC-STUDIO <zcstudio@outlook.com>
2024-09-23 11:23:42 +0000

    Backup: 23 Sep 24, 11:23 AM

--- autostart.sh
+++ autostart.sh
@@ -3,4 +3,4 @@ sleep 5
 cd /workspaces/Ourcraft/server
 /workspaces/Ourcraft/backup.sh &
 /workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 603065, 645050, 645056, 687086 &
-java -Xmx14G -Xms4G -jar /workspaces/Ourcraft/server/paper-1.20.1-196.jar nogui
+java -Xmx12G -jar /workspaces/Ourcraft/server/fabric-server-mc.1.20.1-loader.0.16.5-launcher.1.0.1.jar nogui
